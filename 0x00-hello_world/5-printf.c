2c55bce5248775eb0ee2d09aa00cca4b484e1da2
biigzemira <zemira2609@gmail.com>
2023-07-16 16:22:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A PROGRAMME THAT PRINTS A LINE USING THE PRINTF FUNCTION
+ * Return: 0 always
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
